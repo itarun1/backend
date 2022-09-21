cf7e65c7a2b81a03a53777b89eb3283316fa00f2
itarun1 <95850621+itarun1@users.noreply.github.com>
2022-09-21 16:54:15 +0530

    changes

--- Middleware/authenticate.js
+++ Middleware/authenticate.js
@@ -2,14 +2,16 @@ const jwt= require("jsonwebtoken")
 const User = require("../Model/User.model");
 const authenticate= async(req,res,next)=>{
     try {
-        const token=req.headers.authorization.token
+        const token=req.headers.authorization
         const verifyToken=jwt.verify(token,process.env.JWT_KEY)
-        req.userData = {
-            email: verifyToken.email,
-            userId: verifyToken.userId 
-      };
-        
-        console.log(req,rootUser,"req,rootUser")
+        const rootUser=await User.findOne({_id:verifyToken._id,"token":token})
+        if(!rootUser)
+        {
+            throw new error('User not found')
+        }
+        req.token=token
+        req.user=rootUser
+        req.user._id=rootUser._id
         next();
     } catch (error) {
         res.status(401).send("Unauthorized")
